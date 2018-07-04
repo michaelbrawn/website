ff5e349ef62d7b26615d359a3e885a590b551349
Michael Brawn <michaelbrawn@gmail.com>
2018-07-04 08:15:01 -0400

    Create Credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -107,7 +107,7 @@ end
 
 # Default value for :linked_files is []
 append :linked_files, 'config/secrets.yml.key'
-append :linked_files, 'config/secrets.yml.key'
+append :linked_files, 'config/master.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
